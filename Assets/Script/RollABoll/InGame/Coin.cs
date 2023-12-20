4993b25cb76fec7eea15b2d8c1e5763e4aa59d7f
藤本亜紀 <vga29-ac220062@sankogakuen.jp>
2023-12-20 15:46:51 +0900

    アニメーション

---
+++ Assets/Script/RollABoll/InGame/Coin.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private float bigBonusTime = 1f;
+    private float bonusTime = 2f;
+    private float disappearTime = 5f;
+
+    private int scoreValue = 10;
+    private int bonusScoreValue = 20;
+    private int bigBonusScoreValue = 100;
+
+    public Material CoinMaterial;
+
+    private enum bonusChance
+    {
+        Invalide = -1,
+        BigBonus,
+        SmallBonus,
+        NoBonus
+    }
+
+    private bonusChance bonusChances;
+    void Start()
+    {
+        StartCoroutine(BonusTimeCoroutine());
+    }
+    IEnumerator BonusTimeCoroutine()
+    {
+        bonusChances = bonusChance.BigBonus;
+        CoinMaterial.color = Color.yellow;
+        yield return new WaitForSeconds(bigBonusTime);
+        bonusChances = bonusChance.SmallBonus;
+        CoinMaterial.color = Color.cyan;
+        yield return new WaitForSeconds(bonusTime - bigBonusTime);
+        bonusChances = bonusChance.NoBonus;
+        CoinMaterial.color = Color.red;
+        yield return  new WaitForSeconds(disappearTime - bonusTime);
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Sphere")
+        {
+            switch (bonusChances)
+            {
+                case bonusChance.BigBonus:
+                    ScoreManager.ScoreCount += bigBonusScoreValue;
+                    break;
+                case bonusChance.SmallBonus:
+                    ScoreManager.ScoreCount += bonusScoreValue;
+                    break;
+                case bonusChance.NoBonus:
+                    ScoreManager.ScoreCount += scoreValue;
+                    break;
+            }
+           Destroy(gameObject);
+        }
+    }
+}
