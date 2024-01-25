60b6ee26ce7ccd5322dd9428e5278fdbcf8be51b
藤本亜紀 <vga29-ac220062@sankogakuen.jp>
2024-01-25 12:26:57 +0900

    ジョイスティック

---
+++ Assets/Script/RollABoll/Joistick/JoystickController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class JoystickController : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    // 中心からの最大距離
+    public float maxDistance;
+
+    // クリックを視覚的に表現する
+    public Transform joystickHandle;
+
+    // Vector2を引数に取るUnityEventを作成
+    [Serializable]
+    public class Vector2UnityEvent : UnityEvent<Vector2> { }
+
+    // ジョイスティックの値をXYと-1～1システムで引数に取るイベント
+    public Vector2UnityEvent JoystickOutput;
+
+    // ハンドルのローカル位置の見積もりを計算
+    public Vector3 GetPosition(Vector3 userInput)
+    {
+        Vector3 dir = userInput - transform.position;
+
+        if (Vector3.SqrMagnitude(dir) > maxDistance * maxDistance)
+        {
+            dir = dir.normalized * maxDistance;
+        }
+
+        return dir;
+    }
+
+    // 指が画面上でドラッグされるたびに呼び出される関数
+    public void OnDrag(PointerEventData eventData)
+    {
+        // ジョイスティックハンドルの位置を設定
+        joystickHandle.localPosition = GetPosition(eventData.position);
+
+        // 入力の割合を0～1のスケールで計算
+        Vector2 inputRatio = joystickHandle.localPosition / maxDistance;
+
+        // 値を引数に渡してイベントを送信
+        JoystickOutput?.Invoke(inputRatio);
+    }
+
+    // ユーザーがドラッグをやめたフレームで呼び出される関数
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // 位置を中心にリセットする
+        joystickHandle.localPosition = new Vector3();
+
+        // リスナーに値がnullになったことを通知する
+        JoystickOutput?.Invoke(Vector2.zero);
+    }
+}
